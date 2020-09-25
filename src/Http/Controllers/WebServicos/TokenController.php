8a69a84319c3297661fee64b50e3ba0f40ae60a0
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-09-25 11:48:42 -0300

    Template do PEdreiro para Todos, ajeitando problemas

--- src/Http/Controllers/WebServicos/TokenController.php
+++ src/Http/Controllers/WebServicos/TokenController.php
@@ -212,7 +212,7 @@ class TokenController extends Base
     //  * @param  int  $id
     //  * @return \Illuminate\Http\Response
     //  */
-    // public function show($id)
+    // public function show(Request $request, $id)
     // {
     //     $order = Order::findOrFail($id);
     //     return view('admin.orders.show', compact('order'));
@@ -225,7 +225,7 @@ class TokenController extends Base
     //  * @param  int  $id
     //  * @return \Illuminate\Http\Response
     //  */
-    // public function edit($id)
+    // public function edit(Request $request, $id)
     // {
     //     $order = Order::find($id);
 
@@ -262,7 +262,7 @@ class TokenController extends Base
     //  * @param  int  $id
     //  * @return \Illuminate\Http\Response
     //  */
-    // public function destroy($id)
+    // public function destroy(Request $request, $id)
     // {
     //     $order = Order::findOrFail($id);
     //     $order->delete();
