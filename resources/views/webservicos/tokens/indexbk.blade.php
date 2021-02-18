7fb9785f86a054a4bf97aa9e3462e947a5c626ae
ricardo@sierratecnologia.com.br <ricardo@sierratecnologia.com.br>
2021-02-18 05:17:40 -0300

    CAMN-10

--- resources/views/webservicos/tokens/indexbk.blade.php
+++ resources/views/webservicos/tokens/indexbk.blade.php
@@ -27,14 +27,14 @@
       </div><br />
     @endif
 
-    {{ Form::open(array('method' => 'get', 'route' => array('admin.orders.index'))) }}
+    {!! Form::open(array('method' => 'get', 'route' => array('admin.orders.index')))  !!}
     <div class="input-group input-group-sm">
         {!! Form::text('query', null, ['class' => 'form-control']) !!}
         <span class="input-group-btn">
-            {{ Form::button('Search!', array('class' => 'btn btn-info btn-flat', 'type' => 'submit')) }}
+            {!! Form::button('Search!', array('class' => 'btn btn-info btn-flat', 'type' => 'submit'))  !!}
         </span>
     </div>
-    {{ Form::close() }}
+    {!! Form::close()  !!}
 
     @include('admin.orders.table', ['orders' => $orders])
     {{-- @include('admin.orders.table-ajax') --}}
